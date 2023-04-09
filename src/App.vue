af00ffe0dd612cacd9b9bbf83da0b2c8bdc19662
武晓慧 <374620777@qq.com>
2023-04-09 10:08:39 +0800

    vue3初始化

--- src/App.vue
+++ src/App.vue
@@ -14,10 +14,8 @@ console.log('23')
       width="125"
       height="125"
     />
-
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,10 @@ import './assets/main.css'
 const app = createApp(App)
 
 app.use(createPinia())
+console.log('23')
 
 app.use(router)
 
 app.mount('#app')
+
+// 把我们自己写的代码格式化，别人的代码不要动。
